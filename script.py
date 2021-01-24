3e032d694a06c02d79cf43e6892ebb6c97198639
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2021-01-24 12:44:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,16 +7,16 @@ class script(object):
 
 See /help for more!
 
-©️ <a href="https://t.me/prgofficial">🅼คгςยร  🅷๏ɭɭ๏ฬคץ</a>
+©️ <a href="https://t.me/Mpazaan">@MPAZAAN</a>
     """
     BANNED_USER_TEXT = "Sorry, You're BANNED!"
-    UPGRADE_TEXT = "ping @prgofficial"
+    UPGRADE_TEXT = "ping @Mpazaan"
     REPLY_TO_FILE = "Reply the command to any valid media."
     DOWNLOAD_START = "<b>Give Me Some Time Bruh!!(〒﹏〒)</b>"
     UPLOAD_START = "<b>Hurray... Completed Downloading(;ŏ﹏ŏ)</b>"
     RCHD_BOT_API_LIMIT = "<b>Downloaded in {} seconds.\nDetected File Size: {}\n Sorry. I can't upload above 1.95GB</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = """<b>Job Done 🥳🥳</b>\n\nDownloaded in {} seconds.Uploaded in {} seconds.\n\n©️ <a href="https://t.me/prgofficial">🅼คгςยร  🅷๏ɭɭ๏ฬคץ</a>"""
-    NOT_AUTH_USER_TEXT = """Contact <a href="https://t.me/prgofficial">🅼คгςยร  🅷๏ɭɭ๏ฬคץ</a>"""
+    AFTER_SUCCESSFUL_UPLOAD_MSG = """<b>Job Done 🥳🥳</b>\n\nDownloaded in {} seconds.Uploaded in {} seconds.\n\n©️ <a href="https://t.me/Mpazaan">MPAZAAN</a>"""
+    NOT_AUTH_USER_TEXT = """Contact <a href="https://t.me/Mpazaan">MPAZAAN</a>"""
     SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail Saved ✅ This Is Permanent🥳🥳 \n\nUse /clearthumb for clearing this thumbnail."
     DEL_THUMB = "✅Custom thumbnail cleared successfully."
     SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully😎</b>"
@@ -30,4 +30,12 @@ See /help for more!
 
 3. Send me any Telegram File.
 
-4. Reply to that message with /conv"""
+4. Reply to that message with /conv
+
+5. ആവശ്യമെങ്കിൽ എനിക്ക് ഒരു ലഘുചിത്രം അയയ്ക്കുക. ഇത് ശാശ്വതമായി സംരക്ഷിക്കും
+
+6. ലഘുചിത്രം അയച്ചില്ലെങ്കിൽ, അത് ഫയലിൽ നിന്ന് യാന്ത്രികമായി ജനറേറ്റുചെയ്യും
+
+7. ഏതെങ്കിലും ടെലിഗ്രാം ഫയൽ എനിക്ക് അയയ്ക്കുക.
+ 
+8. ആ സന്ദേശത്തിന് ബോധ്യത്തോടെ /conv മറുപടി നൽകുക"""
